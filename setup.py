7f696d90ca169a2380b6973bf08964a2f8171f45
hengli <github@hengli.me>
2024-04-06 10:54:13 +0000

    init commid

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_package_name',
-    version='1.0.0',
+    name='podd',
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     python_requires='>=3.6',
